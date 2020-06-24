ca83e4b677cbb1d6cafdacb83fa47579c5be3c96
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2020-06-23 22:57:30 -0500

    ECMAScript 7

---
+++ es7.js
@@ -0,0 +1,10 @@
+// ------- ES7 (June 2016) -------
+// Includes
+const numbers = [1, 2, 3, 7, 8];
+console.log(numbers.includes(3));
+console.log(numbers.includes(4));
+
+// Math power
+const base = 9;
+const exponent = 2;
+console.log(base ** exponent);
